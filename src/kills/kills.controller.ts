3070baba18fd31079c0bb597e5e14a3d63d728bd
DanSylvest <cublakhan257@gmail.com>
2024-01-13 21:14:48 +0300

    Add ability to copy system name, changed view of kills and change some icons. also added titles

--- src/kills/kills.controller.ts
+++ src/kills/kills.controller.ts
@@ -1,10 +1,11 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, HttpCode, Post } from '@nestjs/common';
 import { KillsService } from './kills.service';
 
 @Controller('kills')
 export class KillsController {
   constructor(private readonly killsService: KillsService) {}
 
+  @HttpCode(200)
   @Post('systems')
   kills(@Body('systemIds') systemIds: number[]) {
     return this.killsService.getKillsBySystemIds(systemIds);
